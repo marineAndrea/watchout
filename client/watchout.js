6ef988c9c2583ed30965ab8e5884fc873934c069
Marine Guillanton <marine.guillanton@gmail.com>
2015-07-11 10:18:53 -0700

    watchout reorganized

--- client/watchout.js
+++ client/watchout.js
@@ -1,17 +1,4 @@
-// <svg class = "gameboard" width="360px" height="240px"></svg>
-
-// start slingin' some d3 here.
-// define the number of enemies, n
-var n = 1;
-var width = 360;
-var height = 240;
-
-// Dynamically create the board
-d3.select('body').append('svg')
-  .attr('class', 'gameBoard')
-  .attr('width', width)
-  .attr('height', height);
-
+/* ----------------------------- FUNCTION TOOLS -----------------------------*/
 
 var range = function (start, end) {
   var arr = [];
@@ -23,48 +10,30 @@ var range = function (start, end) {
 };
 
 
+/* ----------------------------- THE BOARD -----------------------------*/
+
+// Insert dimensions
+var width = 360;
+var height = 240;
+// Dynamically create the board
+d3.select('body').append('svg')
+  .attr('class', 'gameBoard')
+  .attr('width', width)
+  .attr('height', height);
 
 
-// define super class
+/* ----------------------------- THE SUPERCLASS CHARACTER  -----------------------------*/
+
 var Character = function(name, x, y) {
   this.name = name;
   this.x = x;
   this.y = y;
 };
 
-// define Enemy class with parameters id, xcoord, ycoord
-var Enemy = function(name, x, y) {
-  Character.call(this, name, x, y);
-  this.r = 10;
-  this.color = 'green';
-};
+// !! What about the prototype??
+// it has a prototype that is empty with the exception of the constructor property
 
-Enemy.prototype = Object.create(Character.prototype);
-Enemy.prototype.constructor = Enemy;
-
-// create a User class
-var User = function() {
-  var x = width / 2;
-  var y = height / 2;
-  Character.call(this, 'user', x, y);
-  this.r = 10;
-  this.color = 'red';
-};
-
-User.prototype = Object.create(Character.prototype);
-User.prototype.constructor = User;
-
-
-// Create an array of enemies, each of which is an instance of the class Enemy
-// in order to have d3 get the info on the enemies objects and associate with the DOM
-var enemies = range(0, n).map(function (n) {
-  var x = Math.random() * width;
-  var y = Math.random() * height;
-  return new Enemy(n, x, y);
-});
-
-
-// Add these enemies to the SVG board using d3
+// Add the characters in the DOM to the SVG board using d3
 var addTag = function(circleType, circleData) {
   d3.select('svg').selectAll('circle.' + circleType).data(circleData)
     .enter()
@@ -74,14 +43,70 @@ var addTag = function(circleType, circleData) {
     .attr('cy', function (d) { return d.y })
     .attr('r', function (d) { return d.r })
     .attr('fill', function (d) { return d.color });
-  return d3.select('svg').selectAll('circle.' + circleType);
+  return d3.select('svg').selectAll('circle.' + circleType); // so that we can store the d3 selector
 };
+
+/* ----------------------------- THE ENNEMIES -----------------------------*/
+
+// Insert the number of ennemies
+var n = 1;
+
+// Create the class Enemy that delegetes to the superclass character
+var Enemy = function(name, x, y) {
+  Character.call(this, name, x, y);
+  this.r = 10;
+  this.color = 'green';
+};
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Enemy;
+
+// Create an array of enemies, each of which is an instance of the class Enemy
+// in order to have d3 get the info on the enemies objects and associate it with the DOM
+var enemies = range(0, n).map(function (n) {
+  var x = Math.random() * width;
+  var y = Math.random() * height;
+  return new Enemy(n, x, y);
+});
+
+// EnemiesNodes are d3 objects
+var enemiesNodes = addTag('enemy', enemies);
+
+// Create the function that generates the movement of the enemies
+var moveEnemies = function() {
+  enemies.forEach(function (enemy) {
+    enemy.x = Math.random() * width;
+    enemy.y = Math.random() * height;
+  });
+  return enemiesNodes.data(enemies)
+    .transition()
+    .duration(900)
+    .attr('cx', function (d) { return d.x })
+    .attr('cy', function (d) { return d.y });
+};
+
+// Use setTimeOut to move the enemies
+setInterval(moveEnemies, 1000);
+
+/* ----------------------------- THE USER -----------------------------*/
+
+// Create the class User that delegetes to the superclass character
+var User = function() {
+  var x = width / 2;
+  var y = height / 2;
+  Character.call(this, 'user', x, y);
+  this.r = 10;
+  this.color = 'red';
+};
+User.prototype = Object.create(Character.prototype);
+User.prototype.constructor = User;
 
 var user = new User();
 
-// enemiesNodes and userNode are d3 objects
-var enemiesNodes = addTag('enemy', enemies);
+// UserNode is a d3 object
 var userNode = addTag('user', [user]);
+
+
+/* Make the User move within the board */
 
 var defineEdges = function (d, userTag, direction) {
   var attribute = 'c' + direction;
@@ -114,27 +139,9 @@ var drag = d3.behavior.drag()
     defineEdges(d, userTag, 'x');
     defineEdges(d, userTag, 'y');
   });
-
 userNode.call(drag);
 
-// Create the function that generates the movement
-var moveEnemies = function() {
-  enemies.forEach(function (enemy) {
-    enemy.x = Math.random() * width;
-    enemy.y = Math.random() * height;
-  });
-  return enemiesNodes.data(enemies)
-    .transition()
-    .duration(900)
-    .attr('cx', function (d) { return d.x })
-    .attr('cy', function (d) { return d.y });
-};
-
-// Use setTimeOut that takes a function as argument
-
-setInterval(moveEnemies, 1000);
-
-
+/* ----------------------------- DETECT THE COLLISIONS -----------------------------*/
 
 var detectCollisions = function () {
   // iterate across all the enemies dots
